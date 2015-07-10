4956cc0afea04852c58d0e2a49e34fb25753cd2f
Sam Duvall <sam@rocketwhale.com>
2015-07-10 16:18:22 -0400

    Added convenience orderByAfter function for paging

---
+++ test/lib/query.js
@@ -0,0 +1,43 @@
+var _ = require('underscore');
+var expect = require('chai').expect;
+var Factory = require('../factory');
+var Team = require('../examples/team');
+
+describe('Schema',function() {
+  var teams;
+  beforeEachSync(function() {
+    teams = _.times(5, function(index) {
+      return Factory.create('team', {name: 'Team ' + (index + 1)});
+    });
+  });
+
+  describe('orderByAfter', function() {
+    it('should return the 1st 2 teams', function(done) {
+      Team.query().orderByAfter('name').limit(2).then(function(result) {
+        var names = _.pluck(result, 'name');
+        expect(names).to.eql(['Team 1', 'Team 2']);
+      }).then(done, done);
+    });
+
+    it('should return the 2nd 2 teams', function(done) {
+      Team.query().orderByAfter('teams.name', 'Team 2').limit(2).then(function(result) {
+        var names = _.pluck(result, 'name');
+        expect(names).to.eql(['Team 3', 'Team 4']);
+      }).then(done, done);
+    });
+
+    it('should return the Last 2 teams', function(done) {
+      Team.query().orderByAfter('-name').limit(2).then(function(result) {
+        var names = _.pluck(result, 'name');
+        expect(names).to.eql(['Team 5', 'Team 4']);
+      }).then(done, done);
+    });
+
+    it('should return the 2nd to Last 2 teams', function(done) {
+      Team.query().orderByAfter('-teams.name', 'Team 4').limit(2).then(function(result) {
+        var names = _.pluck(result, 'name');
+        expect(names).to.eql(['Team 3', 'Team 2']);
+      }).then(done, done);
+    });
+  });
+});
