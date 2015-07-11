dd8af89ed88170f5de3c460b17ce5d5826b709b1
Sam Duvall <sam@rocketwhale.com>
2015-07-10 21:53:10 -0400

    Switched orderAfterBy to be just after by id

--- test/lib/query.js
+++ test/lib/query.js
@@ -11,30 +11,30 @@ describe('Schema',function() {
     });
   });
 
-  describe('orderByAfter', function() {
+  describe('after', function() {
     it('should return the 1st 2 teams', function(done) {
-      Team.query().orderByAfter('name').limit(2).then(function(result) {
+      Team.query().after('name').limit(2).then(function(result) {
         var names = _.pluck(result, 'name');
         expect(names).to.eql(['Team 1', 'Team 2']);
       }).then(done, done);
     });
 
     it('should return the 2nd 2 teams', function(done) {
-      Team.query().orderByAfter('teams.name', 'Team 2').limit(2).then(function(result) {
+      Team.query().after('teams.name', 2).limit(2).then(function(result) {
         var names = _.pluck(result, 'name');
         expect(names).to.eql(['Team 3', 'Team 4']);
       }).then(done, done);
     });
 
     it('should return the Last 2 teams', function(done) {
-      Team.query().orderByAfter('-name').limit(2).then(function(result) {
+      Team.query().after('-name').limit(2).then(function(result) {
         var names = _.pluck(result, 'name');
         expect(names).to.eql(['Team 5', 'Team 4']);
       }).then(done, done);
     });
 
     it('should return the 2nd to Last 2 teams', function(done) {
-      Team.query().orderByAfter('-teams.name', 'Team 4').limit(2).then(function(result) {
+      Team.query().after('-teams.name', 4).limit(2).then(function(result) {
         var names = _.pluck(result, 'name');
         expect(names).to.eql(['Team 3', 'Team 2']);
       }).then(done, done);
