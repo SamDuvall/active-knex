3da4d5a2757dab1a1e41bcd3068bf94ee2237404
Sam Duvall <sam@rocketwhale.com>
2016-10-26 17:48:39 +0000

    Added more csv parsing use cases

--- test/lib/schema/fields.js
+++ test/lib/schema/fields.js
@@ -5,56 +5,116 @@ var fields = require('../../../lib/schema/fields');
 describe('Schema Fields',function() {
   describe('known', function() {
     describe('boolean', function() {
-      it('should convert from DB', function() {
-        var fromDb = fields.known.boolean.fromDb(0);
-        expect(fromDb).to.be.false;
+      describe('fromDb', function() {
+        it('should convert 0', function() {
+          var value = fields.known.boolean.fromDb(0);
+          expect(value).to.be.false;
+        });
 
-        var fromDb = fields.known.boolean.fromDb(1);
-        expect(fromDb).to.be.true;
+        it('should convert 1', function() {
+          var value = fields.known.boolean.fromDb(1);
+          expect(value).to.be.true;
+        });
       });
 
-      it('should convert to DB', function() {
-        var toDb = fields.known.boolean.toDb(false);
-        expect(toDb).to.be.false;
+      describe('toDb', function() {
+        it('should convert false', function() {
+          var value = fields.known.boolean.toDb(false);
+          expect(value).to.be.false;
+        });
 
-        var toDb = fields.known.boolean.fromDb(true);
-        expect(toDb).to.be.true;
+        it('should convert true', function() {
+          var value = fields.known.boolean.toDb(true);
+          expect(value).to.be.true;
+        });
       });
     });
 
     describe('csv', function() {
-      it('should convert from DB', function() {
-        var fromDb = fields.known.csv.fromDb('a,b,c');
-        expect(fromDb).to.eql(['a','b','c']);
+      describe('fromDb', function() {
+        it('should convert null', function() {
+          var fromDb = fields.known.csv.fromDb(null);
+          expect(fromDb).to.eql(null);
+        });
+
+        it('should convert empty', function() {
+          var fromDb = fields.known.csv.fromDb('');
+          expect(fromDb).to.eql([]);
+        });
+
+        it('should convert csv', function() {
+          var fromDb = fields.known.csv.fromDb('a,b,c');
+          expect(fromDb).to.eql(['a','b','c']);
+        });
       });
 
-      it('should convert to DB', function() {
-        var toDb = fields.known.csv.toDb(['a','b','c']);
-        expect(toDb).to.eql('a,b,c');
+      describe('toDb', function() {
+        it('should convert null', function() {
+          var fromDb = fields.known.csv.toDb(null);
+          expect(fromDb).to.eql(null);
+        });
+
+        it('should convert empty', function() {
+          var fromDb = fields.known.csv.toDb([]);
+          expect(fromDb).to.eql('');
+        });
+
+        it('should convert array', function() {
+          var fromDb = fields.known.csv.toDb(['a','b','c']);
+          expect(fromDb).to.eql('a,b,c');
+        });
       });
     });
 
-    describe('csvInt', function() {
-      it('should convert from DB', function() {
-        var fromDb = fields.known.csvInteger.fromDb('1,2,3');
-        expect(fromDb).to.eql([1,2,3]);
+    describe('csvInteger', function() {
+      describe('fromDb', function() {
+        it('should convert null', function() {
+          var fromDb = fields.known.csvInteger.fromDb(null);
+          expect(fromDb).to.eql(null);
+        });
+
+        it('should convert empty', function() {
+          var fromDb = fields.known.csvInteger.fromDb('');
+          expect(fromDb).to.eql([]);
+        });
+
+        it('should convert csv', function() {
+          var fromDb = fields.known.csvInteger.fromDb('1,2,3,sdgfsdfg');
+          expect(fromDb).to.eql([1,2,3,NaN]);
+        });
       });
 
-      it('should convert to DB', function() {
-        var toDb = fields.known.csvInteger.toDb([1,2,3]);
-        expect(toDb).to.eql('1,2,3');
+      describe('toDb', function() {
+        it('should convert null', function() {
+          var fromDb = fields.known.csvInteger.toDb(null);
+          expect(fromDb).to.eql(null);
+        });
+
+        it('should convert empty', function() {
+          var fromDb = fields.known.csvInteger.toDb([]);
+          expect(fromDb).to.eql('');
+        });
+
+        it('should convert array', function() {
+          var fromDb = fields.known.csvInteger.toDb([1,2,3]);
+          expect(fromDb).to.eql('1,2,3');
+        });
       });
     });
 
     describe('json', function() {
-      it('should convert from DB', function() {
-        var fromDb = fields.known.json.fromDb('{"foo":"bar"}');
-        expect(fromDb).to.eql({foo: 'bar'});
+      describe('fromDb', function() {
+        it('should convert valid JSON', function() {
+          var fromDb = fields.known.json.fromDb('{"foo":"bar"}');
+          expect(fromDb).to.eql({foo: 'bar'});
+        });
       });
 
-      it('should convert to DB', function() {
-        var toDb = fields.known.json.toDb({foo: 'bar'});
-        expect(toDb).to.eql('{"foo":"bar"}');
+      describe('toDb', function() {
+        it('should convert object', function() {
+          var toDb = fields.known.json.toDb({foo: 'bar'});
+          expect(toDb).to.eql('{"foo":"bar"}');
+        });
       });
     });
   });
